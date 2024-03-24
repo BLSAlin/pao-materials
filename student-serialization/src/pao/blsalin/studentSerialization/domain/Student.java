7f1f1359ab08f1693f1a11fdb3d21d2c62266cdc
Alin Andrei Bălașa <alin.andrei.balasa@gmail.com>
2024-03-24 02:50:26 +0200

    Lab 4 - Skeleton for student serialization

--- student-serialization/src/pao/blsalin/studentSerialization/domain/Student.java
+++ student-serialization/src/pao/blsalin/studentSerialization/domain/Student.java
@@ -2,6 +2,7 @@ package pao.blsalin.studentSerialization.domain;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Student {
     private final String fullName;
@@ -26,5 +27,18 @@ public class Student {
 
     public Map<String, Double> getCourseInformation() {
         return new HashMap<>(courseInformation);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(fullName, student.fullName) && Objects.equals(courseInformation, student.courseInformation);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName, courseInformation);
     }
 }
